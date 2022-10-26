58ae51ba9a91fcc9a663568d8aeff9fcc9531e26
seahow-uk <darmok.and.jalad@tanagra.uk>
2022-10-26 18:16:44 +0100

    first commit

---
+++ ebs-discover-stale-volumes.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python3
+
+"""
+sean@tanagra.uk
+Aug 2022
+
+arguments:
+
+    -r or --region [String]
+        AWS region to use (default is us-east-1)
+
+    -f or --fieldnames [True/False]
+        Whether or not to print a header for the CSV (default is False)
+    
+    -b or --broken [True/False]
+        ONLY return instances where SSM isn't able to see the agent at present
+
+prerequisites:
+
+    pip install boto3
+    pip install argparse
+
+examples (single region):
+
+    python ebs-discover-stale-volumes.py -f True > mycsv.csv
+        Returns list from Virginia with a header and also starts a new CSV (old one will be deleted/overwritten)
+
+    python ebs-discover-stale-volumes.py -r eu-central-1 >> mycsv.csv
+        Returns list from Frankfurt with no header (you don't want a header here because its appending to an existing CSV)
+
+example (multiple regions into one CSV):
+
+    python ebs-discover-stale-volumes.py -r eu-west-1 -f True > mycsv.csv
+    python ebs-discover-stale-volumes.py -r eu-west-2 >> mycsv.csv
+    python ebs-discover-stale-volumes.py -r eu-west-3 >> mycsv.csv
+    python ebs-discover-stale-volumes.py -r eu-north-1 >> mycsv.csv
+
+        The example above puts the data for several European regions into one CSV
+
+"""
+
+import boto3
+import argparse
+from datetime import datetime
+from datetime import timedelta
+
+def setup_args():
+    parser = argparse.ArgumentParser(
+        description='Optional arguments')
+
+    parser.add_argument('-r', '--region',
+                        required=False,
+                        action='store',
+                        help='AWS region to retrieve data from')
+
+    parser.add_argument('-f', '--fieldnames',
+                        required=False,
+                        action='store',
+                        help='Whether to include a CSV header')
+
+    parser.add_argument('-b', '--broken',
+                        required=False,
+                        action='store',
+                        help='ONLY show broken SSM agents')
+
+    return (parser.parse_args())
+
+def main():
+    args = setup_args()
+
+    if args.region:
+        region = str(args.region)
+    else:
+        ## change this if you want to change the region it defaults to
+        region = "us-east-1"
+
+    if args.fieldnames:
+        fieldnames = args.fieldnames
+    else:
+        ## change this to True if you want the header to print by default
+        fieldnames = False
+
+    ## boto3 is the main python sdk for AWS
+    ## you open connections on a per-service basis
+    ec2 = boto3.resource('ec2',region_name=region)
+
+    ## retrieve all ebs volume info in the target region
+    vol_data = ec2.volumes.all()
+
+    ## set up how we want our dates formatted
+    date_format_str = '%B %Y'
+
+    if fieldnames == "True" or fieldnames == "true":
+        ## create header for the CSV but only if the argument -f True was passed
+        print(
+            "Vol ID" + "," +
+            "Vol Name" + "," +
+            "Avail Zone" + "," +
+            "Vol Type" + "," +
+            "Encrypted" + "," +
+            "GB Size" + "," +
+            "Created" + "," +            
+            "Last Attached" + "," + 
+            "Archival Snap?" + "," +
+            "Archived?"
+        )
+
+    ## loop over the list retrieved from ec2
+    for volume in vol_data:
+
+        vol_archived = "False"
+        vol_snapped = "False"
+        vol_name = "unnamed"
+
+        if volume.tags:
+            for t in volume.tags:
+                if t["Key"] == 'Name':
+                    vol_name = t["Value"]
+                if t["Key"] == 'Snapped':
+                    vol_snapped = t["Value"]
+                if t["Key"] == 'Archived':
+                    vol_archived = t["Value"]         
+
+        vol_id = str(volume.id)
+        vol_type = str(volume.volume_type)
+        vol_az = str(volume.availability_zone)
+        vol_size = str(volume.size)
+        vol_state = str(volume.state)
+        vol_encrypted = str(volume.encrypted)
+        vol_created = str(volume.create_time.strftime(date_format_str))
+
+        if (vol_state) == "available":
+
+            print(
+                vol_id + "," +
+                vol_name + "," +
+                vol_az + "," + 
+                vol_type + "," +
+                vol_encrypted + "," +
+                vol_size + "," + 
+                vol_created + "," + 
+                "---" + "," +
+                vol_snapped + "," +
+                vol_archived
+            )
+
+
+if __name__ == "__main__":
+    exit(main())                        
+                    
+                
+                
+
+
